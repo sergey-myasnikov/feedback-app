1f43826d893cb6d818076a32c933b501b00468a7
Carrion Crow <sergey.myasnikov@russia.ru>
2014-11-01 21:38:41 +0400

    basic RESTful functionality

--- src/main/java/feedbackapp/RESTfulController.java
+++ src/main/java/feedbackapp/RESTfulController.java
@@ -1,8 +1,44 @@
 package feedbackapp;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RESTfulController {
+	
+	private FeedbackRepository repository = new FeedbackRepository();
+	
+    @RequestMapping(value="/api", method = RequestMethod.GET)
+    public Feedback helthCheck() {
+        Feedback fb = new Feedback();
+        fb.setId(9999);
+        fb.setName("Sergey Myasnikov");
+        fb.setEmail("sergey@myasnikov@russia.ru");
+        fb.setFeedback("This is a feedback service helthcheck");
+        fb.setIsSpamAgreed(true);
+    	return fb;
+    }
+	
+    @RequestMapping(value="/api/post", method = RequestMethod.POST)
+    public Feedback postFeedback(@RequestBody Feedback fb) {
+    	repository.put(fb);
+        return fb;
+    }
+    
+    @RequestMapping(value="/api/get" , method = RequestMethod.GET)
+    public List<Feedback> getFeedbackList() {
+        return repository.getList();
+    }
+    
+    @RequestMapping(value="/api/get" , method = RequestMethod.DELETE)
+    public List<Feedback> deleteFeedback(@RequestParam long id) {
+    	repository.remove(id);
+        return repository.getList();
+    }
 
 }
