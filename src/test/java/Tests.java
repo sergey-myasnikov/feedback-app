704d3a8cd45e1772d97e060d296245551d368948
Sergey Myasnikov <serj.mjasnikov@gmail.com>
2014-11-02 20:52:48 +0400

    Unit tests update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import feedbackapp.Feedback;
@@ -6,27 +7,34 @@ import static org.junit.Assert.*;
 
 public class Tests {
 	
+	private static Feedback fb1;
+	private static Feedback fb2;
+	private static FeedbackRepository fr;
+	
+	@BeforeClass
+	public static void init() {
+		fb1 = new Feedback();
+		fb2 = new Feedback();		
+		fr = FeedbackRepository.getInstance();	
+	}
+	
+	
     @Test
     public void testFeedbackClass() {
-        Feedback fb = new Feedback();
-        fb.setId(9999);
-        fb.setName("Sergey Myasnikov");
-        fb.setEmail("sergey@myasnikov@russia.ru");
-        fb.setFeedback("This is a feedback service helthcheck");
-        fb.setIsSpamAgreed(true);
-        assertEquals("Wrong ID", fb.getId(), 9999);
-        assertEquals("Wrong name", fb.getName(), "Sergey Myasnikov");
-        assertEquals("Wrong email", fb.getEmail(), "sergey@myasnikov@russia.ru");
-        assertEquals("Wrong feedback text", fb.getFeedback(), "This is a feedback service helthcheck");
-        assertEquals("Wrong spam status", fb.getIsSpamAgreed(), true);
+        fb1.setId(9999);
+        fb1.setName("Sergey Myasnikov");
+        fb1.setEmail("sergey@myasnikov@russia.ru");
+        fb1.setFeedback("This is a feedback service helthcheck");
+        fb1.setIsSpamAgreed(true);
+        assertEquals("Wrong ID", fb1.getId(), 9999);
+        assertEquals("Wrong name", fb1.getName(), "Sergey Myasnikov");
+        assertEquals("Wrong email", fb1.getEmail(), "sergey@myasnikov@russia.ru");
+        assertEquals("Wrong feedback text", fb1.getFeedback(), "This is a feedback service helthcheck");
+        assertEquals("Wrong spam status", fb1.getIsSpamAgreed(), true);
     }
     
     @Test
     public void testFeedbackRepository() {
-    	Feedback fb1 = new Feedback();
-    	Feedback fb2 = new Feedback();
-    	
-    	FeedbackRepository fr = FeedbackRepository.getInstance();
     	fr.put(fb1);
     	fr.put(fb2);
     	assertTrue("Wrond ID assignement", fb2.getId()-fb1.getId() == 1);
