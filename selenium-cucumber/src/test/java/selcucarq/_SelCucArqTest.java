7f46a076128c77b85ca1039606829c969f6ec552
Carrion Crow <sergey.myasnikov@russia.ru>
2014-12-13 02:13:42 +0400

    Update features list

--- selenium-cucumber/src/test/java/selcucarq/_SelCucArqTest.java
+++ selenium-cucumber/src/test/java/selcucarq/_SelCucArqTest.java
@@ -10,7 +10,7 @@ import cucumber.runtime.arquillian.api.Glues;
 
 
 @Glues({HomePageObject.class, FeedbackPageObject.class})
-@Features({"home-page.feature", "feedback-page.feature"})
+@Features({"01_home-page.feature", "02_feedback-page.feature"})
 @CucumberOptions(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 @RunWith(ArquillianCucumber.class)
 public class _SelCucArqTest {
